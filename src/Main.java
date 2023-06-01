a4b1447e6551dd174e8203051d99c3c3a591cb7c
evapomper <eugenia.altunina@gmail.com>
2023-06-01 20:03:20 +0400

    added to new branch

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         double z = 2 * Math.pow(Math.sin(3 * Math.PI - 2 * x), 2) * Math.pow(Math.cos(5 * Math.PI + 2 * x), 2);
         double y = 0.25 - 0.25 * Math.sin(((5 * Math.PI / 2) - 8 * x));
         System.out.printf("%.5f %.5f", z, y);
+        int testBranch;
     }
 }
 
